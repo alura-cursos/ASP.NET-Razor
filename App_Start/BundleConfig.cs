ac894dd4e84a776c7d17a578ac82729d01d07855
andrechavesg <andrechavesg@gmail.com>
2017-11-13 16:02:39 -0200

    capitulo 6

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,7 +8,16 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/style.css");
+            bundles.Add(estilos);
+
+            ScriptBundle scripts = new ScriptBundle("~/bundles/scripts");
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jquery.validate.js");
+            scripts.Include("~/Scripts/jquery.validate.unobtrusive.js");
+            bundles.Add(scripts);
         }
     }
 }
