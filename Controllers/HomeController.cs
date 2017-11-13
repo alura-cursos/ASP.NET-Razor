62ede3cfbfac18666e8f03516ce724d965446fd2
andrechavesg <andrechavesg@gmail.com>
2017-11-13 14:22:37 -0200

    capitulo 1;

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LojaRazor.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            DepartamentosDAO departamentosDAO = new DepartamentosDAO();
+            ProdutosDAO produtosDAO = new ProdutosDAO();
+            ViewBag.Departamentos = departamentosDAO.Lista();
+            ViewBag.Produtos = produtosDAO.Ofertas();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
